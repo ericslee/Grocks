881220c7ec4a184cfaef14e266c74c608b552490
Gary <likangning93@gmail.com>
2014-03-29 17:56:14 -0400

    added experimental physics handler stuff that seems to work. all marked as experimental

---
+++ Assets/Scenes/box_collision_experiment.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class box_collision_experiment : MonoBehaviour {
+
+	public int HP;
+	private float HPmax;
+	private GameObject sitter;
+	
+	// Use this for initialization
+	void Start () {
+		HPmax = HP;
+		if (HP == 0) HPmax = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// presumably we'll put differing render code here?
+		// right now the block will just get darker the more times it's struck
+		gameObject.renderer.material.color = new Color(HP/HPmax,HP/HPmax,HP/HPmax);
+
+	}
+	
+	void OnCollisionEnter(Collision hit) {
+		/* calculate difference between center of this and center of collision object */
+		// get center point Ys
+		sitter = hit.gameObject;
+		if (HP == 0) {
+			Destroy (gameObject);
+			return;
+		}
+
+		float otherY = hit.transform.position.y;
+		float thisY = this.gameObject.transform.position.y;
+		// get heights of objects
+		float otherH = hit.transform.localScale.y / 2;
+		float thisH = this.transform.localScale.y / 2;
+		
+		bool collidingOnSide = false;
+		
+		if (Mathf.Abs(thisY - otherY) + 0.1 < thisH + otherH) {
+			Debug.Log(thisH + otherH + " height difference"); // TODO
+			Debug.Log(Mathf.Abs(thisY - otherY) + 0.1 + " centerpoint difference"); // TODO
+			collidingOnSide = true;
+		}
+
+		if (!collidingOnSide) {
+			HP--;
+			sitter.GetComponent<player_experiment>().inAir = false;
+		}
+	}
+}
---
+++ Assets/Scenes/player_experiment.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player_experiment : MonoBehaviour {
+	private int dir; // -1 is facing upside down (on the ceiling), 1 is facing rightside up (on the floor)
+	public bool inAir;
+	private Vector3 leftSpeed = new Vector3(0, 0, -30.0f);
+	private Vector3 rightSpeed = new Vector3(0, 0, 30.0f);
+	public float bounceTimer;
+
+	// Use this for initialization
+	void Start () {
+		dir = -1;
+		inAir = false;
+		bounceTimer = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		bounceTimer--;
+		if (bounceTimer < 0)
+			bounceTimer = 0.0f;
+
+		if (!inAir) {
+			rigidbody.velocity = Vector3.zero;
+		}
+		if (inAir) rigidbody.AddForce(0, dir * 300.0f, 0);
+
+
+		if (Input.GetKey ("a") && inAir && bounceTimer < 1) {
+			rigidbody.MovePosition(rigidbody.position + leftSpeed * Time.deltaTime);
+			
+			// remove horizontal velocity if player moves in air after collision
+			rigidbody.velocity = new Vector3(0, rigidbody.velocity.y, 0);
+		}
+		// move left and right
+		if (Input.GetKey ("d") && inAir && bounceTimer < 1) {
+			rigidbody.MovePosition(rigidbody.position + rightSpeed * Time.deltaTime);
+			
+			// remove horizontal velocity if player moves in air after collision
+			rigidbody.velocity = new Vector3(0, rigidbody.velocity.y, 0);
+		}
+
+		if (Input.GetKeyDown ("w") && !inAir && dir < 0) {
+			//rigidbody.AddForce(0, 500.0f, 0);
+			dir = 1;
+			inAir = true;
+		}
+		if (Input.GetKeyDown ("s") && !inAir && dir > 0) {
+			//rigidbody.AddForce(0, -500.0f, 0);
+			dir = -1;
+			inAir = true;
+		}
+	}
+
+	void OnCollisionEnter (Collision hit) {
+
+		float otherY = hit.transform.position.y;
+		float thisY = this.gameObject.transform.position.y;
+		// get heights of objects
+		float otherH = hit.transform.localScale.y / 2;
+		float thisH = this.transform.localScale.y / 2;
+		
+		bool collidingOnSide = false;
+		
+		if (Mathf.Abs(thisY - otherY) + 0.1 < thisH + otherH) {
+			Debug.Log(thisH + otherH + " height difference"); // TODO
+			Debug.Log(Mathf.Abs(thisY - otherY) + 0.1 + " centerpoint difference"); // TODO
+			collidingOnSide = true;
+		}
+	}
+
+	void OnTriggerEnter (Collider other) {
+		// bounce away
+		if(other.tag == "LeftBouncer") {
+			rigidbody.AddForce(0, 0.0f, -2000.0f);
+			bounceTimer = 2000.0f;
+		}
+		else if (other.tag == "RightBouncer") {
+			rigidbody.AddForce(0, 0.0f, 2000.0f);
+			bounceTimer = 2000.0f;
+		}
+	}
+
+	void OnCollisionExit (Collision collision) {
+		inAir = true;
+	}
+}
