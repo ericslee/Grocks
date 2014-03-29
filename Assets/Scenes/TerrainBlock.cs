f4ef558ef9446ec3f86f5c7651aad8fb6e7443f5
Gary <likangning93@gmail.com>
2014-03-28 23:02:49 -0400

    added terrain block HP script

--- Assets/Scenes/TerrainBlock.cs
+++ Assets/Scenes/TerrainBlock.cs
@@ -3,13 +3,27 @@ using System.Collections;
 
 public class TerrainBlock : MonoBehaviour {
 
+	public int HP;
+	private float HPmax;
+
 	// Use this for initialization
 	void Start () {
-	
+		HPmax = HP;
+		if (HP == 0) HPmax = 1;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		// presumably we'll put differing render code here?
+		// right now the block will just get darker the more times it's struck
+		gameObject.renderer.material.color = new Color(HP/HPmax,HP/HPmax,HP/HPmax);
+
+		if (HP == 0) {
+			Destroy (gameObject);
+		}
+	}
+
+	void OnTriggerEnter(Collider youHitMe) {
+		HP--;
 	}
 }
