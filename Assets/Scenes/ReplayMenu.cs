8eca1e7ac1879184e9152deeef0ba24e30f035ab
Judy Trinh <jtrinh74@gmail.com>
2014-03-29 16:19:12 -0400

    Switched to actual GUI and GUISkin for replay because it makes more sense. Will delete restart things in next commit

---
+++ Assets/Scenes/ReplayMenu.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReplayMenu : MonoBehaviour {
+	
+	public GUISkin customSkin;
+	private GameManager gameManager;
+
+	int screenCenterX = Screen.width/2;
+	int screenCenterY = Screen.height/2;
+	int buttonWidth = 100;
+	int buttonHeight = 50;
+	int titleWidth = 335;
+	int titleHeight = 150;
+	
+	// Use this for initialization
+	void Start () {
+		gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Show() {
+		enabled = true;
+	}
+
+	public void Hide() {
+		enabled = false;
+	}
+	
+	void OnGUI() {
+		
+		GUI.skin = customSkin;
+		
+		GUI.Label(new Rect (screenCenterX-titleWidth/2, screenCenterY-titleHeight/2, titleWidth, titleHeight), 
+		          "Player 1 wins!");
+		
+		if(GUI.Button(new Rect(screenCenterX-buttonWidth/2, 265, buttonWidth, buttonHeight), "Replay")) {
+			gameManager.Reset();
+		}
+		
+		if(GUI.Button(new Rect(screenCenterX-buttonWidth/2, 325, buttonWidth, buttonHeight), "Quit")) {
+			Application.Quit();
+		}
+	}
+}
