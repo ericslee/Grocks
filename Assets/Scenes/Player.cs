1ea3ac212f3c1379fd8c04725095d6fcbba4639e
Judy Trinh <jtrinh74@gmail.com>
2014-03-29 00:41:40 -0400

    In progress on gravity flipping, needs work for flipping back

--- Assets/Scenes/Player.cs
+++ Assets/Scenes/Player.cs
@@ -6,23 +6,27 @@ public class Player : MonoBehaviour {
 	private Vector3 leftSpeed = new Vector3(0, 0, -15.0f);
 	private Vector3 rightSpeed = new Vector3(0, 0, 15.0f);
 
-	public bool inAir;
-	int dir;
+	private bool inAir;
+	private int dir; // -1 is facing upside down (on the ceiling), 1 is facing rightside up (on the floor)
+	private bool changedGravity;
 
 	// Use this for initialization
 	void Start () {
 		inAir = false;
 		dir = 1;
+		changedGravity = false;
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 		// add force on button press
-		if (Input.GetKeyDown ("w")) {
+		if (Input.GetKeyDown ("w") && !changedGravity) {
 			rigidbody.AddForce(0, 100.0f, 0);
+			changedGravity = true;
 		}
-		if (Input.GetKeyDown ("s")) {
+		if (Input.GetKeyDown ("s") && !changedGravity) {
 			rigidbody.AddForce(0, -100.0f, 0);
+			changedGravity = true;
 		}
 
 		// move left and right
@@ -32,6 +36,19 @@ public class Player : MonoBehaviour {
 		// move left and right
 		if (Input.GetKey ("d")) {
 			rigidbody.MovePosition(rigidbody.position + rightSpeed * Time.deltaTime);
+		}
+	}
+
+	void OnCollisionEnter (Collision collision) {
+		if (collision.gameObject.tag == "Ceiling") {
+			dir = -1;
+			changedGravity = false;
+			rigidbody.velocity = Vector3.zero;
+		}
+		else if (collision.gameObject.tag == "Floor") {
+			dir = 1;
+			changedGravity = false;
+			rigidbody.velocity = Vector3.zero;
 		}
 	}
 }
