34572c025064ce60fca91af10cbae9149ef666dd
Judy Trinh <jtrinh74@gmail.com>
2014-03-29 13:12:55 -0400

    Added functions in game manager for creating/destroying restart menu

--- Assets/Scenes/GameManager.cs
+++ Assets/Scenes/GameManager.cs
@@ -3,11 +3,23 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour {
 
+	public GameObject restartMenuPrefab;
+	private GameObject restartMenu;
+
 	// Use this for initialization
 	void Start () {
-	
+
 	}
-	
+
+	// TODO: when brought into main arena, attach prefab to public var in inspector
+	void CreateRestartMenu() {
+		restartMenu = (GameObject)(GameObject.Instantiate(restartMenuPrefab));
+	}
+
+	void DestroyRestartMenu() {
+		Destroy(restartMenu);
+	}
+
 	// Update is called once per frame
 	void Update () {
 	
