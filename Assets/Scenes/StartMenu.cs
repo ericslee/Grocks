46f466be6b78ad0f4ab628eae51716223aabe278
Judy Trinh <jtrinh74@gmail.com>
2014-03-29 14:40:59 -0400

    Started basic start menu GUI

--- Assets/Scenes/StartMenu.cs
+++ Assets/Scenes/StartMenu.cs
@@ -5,11 +5,24 @@ public class StartMenu : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void OnGUI() {
+		// Make a background box
+		GUI.Box(new Rect(10,10,100,90), "START MENU");
+
+		if(GUI.Button(new Rect(20,40,80,20), "Start")) {
+			Application.LoadLevel("Arena_1");
+		}
+
+		if(GUI.Button(new Rect(20,70,80,20), "Quit")) {
+			Application.Quit();
+		}
 	}
 }
