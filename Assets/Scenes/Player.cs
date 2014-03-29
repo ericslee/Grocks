1c529a832ca077d41727c4d5a451c9603aabc28d
Eric Sun Lee <ericslee@seas.upenn.edu>
2014-03-29 11:25:46 -0400

    Acceleration in the air

--- Assets/Scenes/Player.cs
+++ Assets/Scenes/Player.cs
@@ -3,8 +3,8 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
-	private Vector3 leftSpeed = new Vector3(0, 0, -15.0f);
-	private Vector3 rightSpeed = new Vector3(0, 0, 15.0f);
+	private Vector3 leftSpeed = new Vector3(0, 0, -30.0f);
+	private Vector3 rightSpeed = new Vector3(0, 0, 30.0f);
 
 	private bool inAir;
 	private int dir; // -1 is facing upside down (on the ceiling), 1 is facing rightside up (on the floor)
@@ -24,6 +24,11 @@ public class Player : MonoBehaviour {
 			rigidbody.velocity = Vector3.zero;
 		}
 
+		// constantly apply to vertical force when in the air
+		if (inAir) {
+			rigidbody.AddForce(0, dir * 200.0f, 0);
+		}
+
 		// add force on button press
 		if (Input.GetKeyDown ("w") && !changedGravity) {
 			rigidbody.AddForce(0, 500.0f, 0);
@@ -39,10 +44,16 @@ public class Player : MonoBehaviour {
 		// move left and right
 		if (Input.GetKey ("a") && inAir) {
 			rigidbody.MovePosition(rigidbody.position + leftSpeed * Time.deltaTime);
+
+			// remove horizontal velocity if player moves in air after collision
+			rigidbody.velocity = new Vector3(0, rigidbody.velocity.y, 0);
 		}
 		// move left and right
 		if (Input.GetKey ("d") && inAir) {
 			rigidbody.MovePosition(rigidbody.position + rightSpeed * Time.deltaTime);
+
+			// remove horizontal velocity if player moves in air after collision
+			rigidbody.velocity = new Vector3(0, rigidbody.velocity.y, 0);
 		}
 	}
 
