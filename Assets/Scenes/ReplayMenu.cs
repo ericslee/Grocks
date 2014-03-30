c4d766f5ec2a7a078b2c84d63e9d3c35b5cea50b
Judy Trinh <jtrinh74@gmail.com>
2014-03-29 23:09:56 -0400

    Fixed line overflow on replay label

--- Assets/Scenes/ReplayMenu.cs
+++ Assets/Scenes/ReplayMenu.cs
@@ -5,14 +5,14 @@ public class ReplayMenu : MonoBehaviour {
 	
 	public GUISkin customSkin;
 	private GameManager gameManager;
-	private string currentWinner; 
+	private string currentWinner;
 
 	int screenCenterX = Screen.width/2;
 	int screenCenterY = Screen.height/2;
 	int buttonWidth = 100;
 	int buttonHeight = 50;
-	int titleWidth = 335;
-	int titleHeight = 150;
+	int titleWidth = 350;
+	int titleHeight = 50;
 	
 	// Use this for initialization
 	void Start () {
@@ -43,11 +43,11 @@ public class ReplayMenu : MonoBehaviour {
 		GUI.Label(new Rect (screenCenterX-titleWidth/2, screenCenterY-titleHeight/2, titleWidth, titleHeight), 
 		          currentWinner + " wins!");
 		
-		if(GUI.Button(new Rect(screenCenterX-buttonWidth/2, 265, buttonWidth, buttonHeight), "Replay")) {
+		if(GUI.Button(new Rect(screenCenterX-buttonWidth/2, screenCenterY+titleHeight/2 + buttonHeight/2, buttonWidth, buttonHeight), "Replay")) {
 			gameManager.Reset();
 		}
 		
-		if(GUI.Button(new Rect(screenCenterX-buttonWidth/2, 325, buttonWidth, buttonHeight), "Quit")) {
+		if(GUI.Button(new Rect(screenCenterX-buttonWidth/2, screenCenterY+titleHeight/2 + buttonHeight/2 + 55, buttonWidth, buttonHeight), "Quit")) {
 			Application.Quit();
 		}
 	}
